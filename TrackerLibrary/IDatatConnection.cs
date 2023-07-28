9b3dddeac6f38e82d111da562c45ff1a675b73f3
PrakGov <55034062+prak112@users.noreply.github.com>
2023-07-28 20:16:10 +0300

    prelim-define interface and member

---
+++ TrackerLibrary/IDatatConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public interface IDatatConnection //every item inside Interface are public 
+    {
+        PrizeModel CreatePrize(PrizeModel model);
+    }
+}
