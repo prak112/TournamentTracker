748262791a3072e19af944520f7904f58a5797ea
PrakGov <55034062+prak112@users.noreply.github.com>
2023-07-28 20:16:44 +0300

    update model to process incoming data

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -8,6 +8,35 @@ namespace TrackerLibrary
 {
     public class PrizeModel
     {
+        public PrizeModel()
+        {
+            
+        }
+
+        public PrizeModel(string position, string positionName, string prizeAmount, string prizePercentage)
+        {
+
+            int positionValue = 0;
+            int.TryParse(position, out positionValue);
+            Position = positionValue;
+
+            PositionName = positionName;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+
+        }
+
+        /// <summary>
+        /// Represents unique identifier for every prize
+        /// </summary>
+        public int Id { get; set; }
+
         /// <summary>
         /// Represents position of team
         /// </summary>
@@ -19,13 +48,14 @@ namespace TrackerLibrary
         public string PositionName { get; set; }
         
         /// <summary>
-        /// Represents prize money for team
+        /// Represents prize money for team, either 0 or some amount
         /// </summary>
         public decimal PrizeAmount { get; set; }
         
         /// <summary>
-        /// Represents percentage of prize money for team
+        /// Percentage of prize money for team, represented as fraction of 1, i.e., 0.1 is 10%
+        /// It is either 0 or some fraction
         /// </summary>
-        public string PrizePercentage { get;set; }
+        public double PrizePercentage { get;set; }
     }
 }
