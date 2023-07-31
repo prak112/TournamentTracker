d05a3f7a3e3e9653f743b88a9dee645d7eed217f
PrakGov <55034062+prak112@users.noreply.github.com>
2023-07-31 20:17:13 +0300

    add method to connect database

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace TrackerLibrary
         /// </summary>
         public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
+
         /// <summary>
         /// To intialize all possible data source connections for data storage
         /// </summary>
@@ -28,7 +30,6 @@ namespace TrackerLibrary
                 SqlConnector sqlConnector = new SqlConnector();
                 Connections.Add(sqlConnector);
             }
-
             if (textFiles)
             {
                 // TODO - Setup TextFileConnector
@@ -36,5 +37,19 @@ namespace TrackerLibrary
                 Connections.Add(textFileConnector);
             }
         }
+
+        
+        /// <summary>
+        /// To connect to specific database
+        /// </summary>
+        /// <param name="name">Name of the database</param>
+        /// <returns>Connection string to connect with the database</returns>
+        public static string ConnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+        }
     }
+
+    
 }
