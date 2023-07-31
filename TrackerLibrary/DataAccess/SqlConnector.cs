90c529873a2e902328e4bd0227ae5578d37f873c
PrakGov <55034062+prak112@users.noreply.github.com>
2023-07-31 14:20:39 +0300

    move files to DataAccess dir

---
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class SqlConnector : IDataConnection
+    {
+        // TODO - Create database connection to save prize information 
+        /// <summary>
+        /// Stores new prize information to database
+        /// </summary>
+        /// <param name="model">prize information</param>
+        /// <returns>prize information including unique identifier</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
---
+++ TrackerLibrary/DataAccess/TextFileConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class TextFileConnector : IDataConnection
+    {
+        // TODO - Implement method and use Linq to save CreatePrize information to text file
+        /// <summary>
+        /// Stores prize information to text file 
+        /// </summary>
+        /// <param name="model">saves prize information</param>
+        /// <returns>prize information including unique identifier</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            // sample code
+            model.Id = 1;
+            
+            return model;
+        }
+    }
+}
