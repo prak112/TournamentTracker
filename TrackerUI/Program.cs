cbaca8eaf9532ac684e418ab44306b3a3f16d01a
PrakGov <55034062+prak112@users.noreply.github.com>
2023-08-07 17:06:18 +0300

    change app entry point for test

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -21,7 +21,7 @@ namespace TrackerUI
             // Initialize database connection - default option SqlDb
             TrackerLibrary.GlobalConfig.InitializeConnection(DatabaseType.TextFile);
 
-            Application.Run(new CreatePrizeForm());
+            Application.Run(new CreateTeamForm());
             //Application.Run(new TournamentDashboardForm());
         }
     }
