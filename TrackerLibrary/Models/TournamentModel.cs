3c2b0caaabc25bfc427ed2175c1067f6e16bafc0
PrakGov <55034062+prak112@users.noreply.github.com>
2023-08-21 21:19:13 +0300

    add Id prop, format summaries

--- TrackerLibrary/Models/TournamentModel.cs
+++ TrackerLibrary/Models/TournamentModel.cs
@@ -9,29 +9,34 @@ namespace TrackerLibrary.Models
     public class TournamentModel
     {
         /// <summary>
-        /// Represents name of tournament, sport-gender such as volleyball-women
+        /// Unique identifier for every tournament entry
+        /// </summary>
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// tournament name with sport-gender such as volleyball-women
         /// </summary>
         public string TournamentName { get; set; }
         
         /// <summary>
-        /// Represents amount charged to team to enter tournament
+        /// amount charged to team to enter tournament
         /// </summary>
         public decimal EntryFee { get; set; }
         
         /// <summary>
-        /// Represents list of teams entered
+        /// list of teams entered
         /// </summary>
         public List<TeamModel> Teams { get; set; } = new List<TeamModel>();
         
         //public List<MatchModel> Matches { get; set; } = new List<MatchModel>();
         
         /// <summary>
-        /// Represents list of prizes for winner or podium positions
+        /// list of prizes for winner or podium positions
         /// </summary>
         public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
         
         /// <summary>
-        /// Represents list of matches in current round
+        /// list of matches in current round
         /// </summary>
         public List<List<MatchModel>> Rounds { get; set; } = new List<List<MatchModel>>(); 
     }
