4485f9ba47ce124860bc519d59288260b161d87b
PrakGov <55034062+prak112@users.noreply.github.com>
2023-09-03 19:02:50 +0300

    finalize steps to wire match models

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -212,12 +212,8 @@ namespace TrackerUI
                 tournament.Prizes = selectedPrizes;
                 tournament.Teams = selectedTeams;
 
-                // TODO - Wire MatchModels
-                // Team lists randomization - to prevent systematic or biased match modeling
-                // Verify Randomized team lists for match modeling, IF insufficient add Byes - 2^n Teams required for smooth match modeling
-                // Create 1st MatchRound
-                // Create every round with Match Ids from MatchRound 1
-
+                // Wire MatchModels
+                TournamentLogic.CreateRounds(tournament);
 
                 // pass model data to sql server
                 GlobalConfig.Connection.CreateTournament(tournament);
